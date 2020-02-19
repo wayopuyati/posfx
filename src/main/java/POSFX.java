734aa3627274bb670ba5205b07fe0f8c764cffe9
wichit2s <wichit.s@ubu.ac.th>
2020-02-20 01:04:41 +0700

    setting layout for product

--- src/main/java/POSFX.java
+++ src/main/java/POSFX.java
@@ -1,9 +1,17 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.swing.border.BevelBorder;
 
 import org.kordamp.bootstrapfx.scene.layout.Panel;
 
@@ -22,7 +30,24 @@ public class POSFX extends Application {
         button.setOnAction( (e) -> {
             stage.close();
         });
-        content.setCenter(button);
+        GridPane productsView = new GridPane();
+        productsView.setHgap(8.0);
+        productsView.setVgap(5.0);
+        productsView.setPadding(new Insets(15.0));
+        for (int r=0; r<3; r++) {
+            for (int c=0; c<3; c++) {
+                productsView.add(new ImageView("https://picsum.photos/190"), c, r);
+            }
+        }
+        ScrollPane viewPane = new ScrollPane(productsView);
+        content.setCenter(viewPane);
+        HBox hbox = new HBox(10);
+        hbox.getStyleClass().setAll("panel", "panel-body");
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().add(new Button("Sale"));
+        hbox.getChildren().add(new Button("Admin"));
+        hbox.getChildren().add(button);
+        content.setBottom(hbox);
         panel.setBody(content);
 
         Scene scene = new Scene(panel, 640, 480);
